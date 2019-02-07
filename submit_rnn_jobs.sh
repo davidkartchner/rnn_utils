8fe78149c05ac5df931f8561e9a3a197279c599c
davidkartchner <davidkartchner@gmail.com>
2019-02-07 10:34:16 -0500

    correct rnn flags

--- submit_rnn_jobs.sh
+++ submit_rnn_jobs.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-python 1_train_rnn.py data_path data/structured_0_months --save data/results/structured_0_months
+python 1_train_rnn.py --save data/results/structured_0_months data/structured_0_months
+# python 1_train_rnn.py --save data/results/structured_3_months data/structured_3_months
+# python 1_train_rnn.py --save data/results/structured_6_months data/structured_6_months
+# python 1_train_rnn.py --save data/results/structured_9_months data/structured_9_months
+# python 1_train_rnn.py --save data/results/structured_12_months data/structured_12_months
+
+# python 1_train_rnn.py data/notes_0_months --save data/results/notes_0_months
+# python 1_train_rnn.py data/notes_3_months --save data/results/notes_3_months
+# python 1_train_rnn.py data/notes_6_months --save data/results/notes_6_months
+# python 1_train_rnn.py data/notes_9_months --save data/results/notes_9_months
+# python 1_train_rnn.py data/notes_12_months --save data/results/notes_12_months
+
+# python 1_train_rnn.py data/structured_with_notes_0_months --save data/results/structured_with_notes_0_months
+# python 1_train_rnn.py data/structured_with_notes_3_months --save data/results/structured_with_notes_3_months
+# python 1_train_rnn.py data/structured_with_notes_6_months --save data/results/structured_with_notes_6_months
+# python 1_train_rnn.py data/structured_with_notes_9_months --save data/results/structured_with_notes_9_months
+# python 1_train_rnn.py data/structured_with_notes_12_months --save data/results/structured_with_notes_12_months
